9079cfffbef35f04b0a10a75d6ba0fb93876b012
AlexHaxe <Alexander.Blum@gmail.com>
2020-04-13 21:05:54 +0200

    run some targets twice to smooth out their graphs
    remove crypto lib for sha256 run - use Haxe std instead

--- benchmarkBCrypt.sh
+++ benchmarkBCrypt.sh
@@ -18,3 +18,14 @@ php out/php/index.php $DATA
 neko out/BCryptBenchmark.n $DATA
 # python3 out/BCryptBenchmark.py $DATA
 # lua out/BCryptBenchmark.lua $DATA
+
+# second run to smooth graphs a little
+./out/cpp/BCryptBenchmark $DATA
+./out/cppGCGen/BCryptBenchmark $DATA
+node out/BCryptBenchmark.js $DATA
+node out/BCryptBenchmark.es6.js $DATA
+java -jar out/java/BCryptBenchmark.jar $DATA
+java -jar out/jvm/BCryptBenchmark.jar $DATA
+hl out/BCryptBenchmark.hl $DATA
+./out/hl/BCryptBenchmark $DATA
+mono out/cs/bin/BCryptBenchmark.exe $DATA
--- benchmarkSHA256.sh
+++ benchmarkSHA256.sh
@@ -18,3 +18,16 @@ php out/php/index.php $DATA
 neko out/Sha256Benchmark.n $DATA
 # python3 out/Sha256Benchmark.py $DATA
 # lua out/Sha256Benchmark.lua $DATA
+
+# second run to smooth graphs a little
+./out/cpp/Sha256Benchmark $DATA
+./out/cppGCGen/Sha256Benchmark $DATA
+node out/Sha256Benchmark.js $DATA
+node out/Sha256Benchmark.es6.js $DATA
+java -jar out/java/Sha256Benchmark.jar $DATA
+java -jar out/jvm/Sha256Benchmark.jar $DATA
+hl out/Sha256Benchmark.hl $DATA
+./out/hl/Sha256Benchmark $DATA
+mono out/cs/bin/Sha256Benchmark.exe $DATA
+haxe build/sha256/common.hxml --run Sha256Benchmark $DATA
+php out/php/index.php $DATA
