47f23cfba4a8d06f5da25c9c15413ffb0a643d1f
AlexHaxe <Alexander.Blum@gmail.com>
2020-04-13 15:11:08 +0200

    fixed pacths

--- benchmarkSHA256.sh
+++ benchmarkSHA256.sh
@@ -4,17 +4,17 @@ source benchmark.env
 
 readonly DATA=$*
 
-./out/cpp/Sha512Benchmark $DATA
-./out/cppGCGen/Sha512Benchmark $DATA
-node out/Sha512Benchmark.js $DATA
-node out/Sha512Benchmark.es6.js $DATA
-java -jar out/java/Sha512Benchmark.jar $DATA
-java -jar out/jvm/Sha512Benchmark.jar $DATA
-hl out/Sha512Benchmark.hl $DATA
-./out/hl/Sha512Benchmark $DATA
-mono out/cs/bin/Sha512Benchmark.exe $DATA
-haxe build/sha512/common.hxml --run Sha512Benchmark $DATA
+./out/cpp/Sha256Benchmark $DATA
+./out/cppGCGen/Sha256Benchmark $DATA
+node out/Sha256Benchmark.js $DATA
+node out/Sha256Benchmark.es6.js $DATA
+java -jar out/java/Sha256Benchmark.jar $DATA
+java -jar out/jvm/Sha256Benchmark.jar $DATA
+hl out/Sha256Benchmark.hl $DATA
+./out/hl/Sha256Benchmark $DATA
+mono out/cs/bin/Sha256Benchmark.exe $DATA
+haxe build/sha256/common.hxml --run Sha256Benchmark $DATA
 php out/php/index.php $DATA
-neko out/Sha512Benchmark.n $DATA
-python3 out/Sha512Benchmark.py $DATA
-# lua out/Sha512Benchmark.lua $DATA
+neko out/Sha256Benchmark.n $DATA
+python3 out/Sha256Benchmark.py $DATA
+# lua out/Sha256Benchmark.lua $DATA
--- build/sha256/Build.hx
+++ build/sha256/Build.hx
@@ -9,18 +9,18 @@ class Build {
 		run all build files
 	**/
 	public static function main() {
-		callLix("build/sha512/buildCpp.hxml", "C++");
-		callLix("build/sha512/buildCppGCGen.hxml", "C++ (HXCPP_GC_GENERATIONAL)");
-		callLix("build/sha512/buildCs.hxml", "C#");
-		callLix("build/sha512/buildJava.hxml", "Java");
-		callLix("build/sha512/buildJvm.hxml", "JVM");
-		callLix("build/sha512/buildHl.hxml", "HL/Jit");
-		callLix("build/sha512/buildHlc.hxml", "HL/C");
-		callLix("build/sha512/buildJs.hxml", "JS/ES5");
-		callLix("build/sha512/buildJsES6.hxml", "JS/ES6");
-		callLix("build/sha512/buildNeko.hxml", "Neko");
-		callLix("build/sha512/buildPhp.hxml", "PHP7");
-		callLix("build/sha512/buildPython.hxml", "Python");
+		callLix("build/sha256/buildCpp.hxml", "C++");
+		callLix("build/sha256/buildCppGCGen.hxml", "C++ (HXCPP_GC_GENERATIONAL)");
+		callLix("build/sha256/buildCs.hxml", "C#");
+		callLix("build/sha256/buildJava.hxml", "Java");
+		callLix("build/sha256/buildJvm.hxml", "JVM");
+		callLix("build/sha256/buildHl.hxml", "HL/Jit");
+		callLix("build/sha256/buildHlc.hxml", "HL/C");
+		callLix("build/sha256/buildJs.hxml", "JS/ES5");
+		callLix("build/sha256/buildJsES6.hxml", "JS/ES6");
+		callLix("build/sha256/buildNeko.hxml", "Neko");
+		callLix("build/sha256/buildPhp.hxml", "PHP7");
+		callLix("build/sha256/buildPython.hxml", "Python");
 	}
 
 	/**
@@ -31,7 +31,7 @@ class Build {
 	**/
 	public static function callLix(buildFile:String, title:String) {
 		var startTime = Timer.stamp();
-		var exitCode:Int = Sys.command("npx", ["haxe", "build/sha512/common.hxml", buildFile]);
+		var exitCode:Int = Sys.command("npx", ["haxe", "build/sha256/common.hxml", buildFile]);
 		var exitText:String = "";
 		if (exitCode != 0) {
 			exitText = ' - exitCode: $exitCode';
