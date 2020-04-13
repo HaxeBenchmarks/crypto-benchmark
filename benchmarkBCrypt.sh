c80ecf1d8e324b30d55cecf5d0bc771b735d7800
AlexHaxe <Alexander.Blum@gmail.com>
2020-04-13 10:56:21 +0200

    fixed run script

---
+++ benchmarkBCrypt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source benchmark.env
+
+readonly DATA=$*
+
+./out/cpp/BCryptBenchmark $DATA
+./out/cppGCGen/BCryptBenchmark $DATA
+node out/BCryptBenchmark.js $DATA
+node out/BCryptBenchmark.es6.js $DATA
+java -jar out/java/BCryptBenchmark.jar $DATA
+java -jar out/jvm/BCryptBenchmark.jar $DATA
+hl out/BCryptBenchmark.hl $DATA
+./out/hl/BCryptBenchmark $DATA
+mono out/cs/bin/BCryptBenchmark.exe $DATA
+haxe build/common.hxml --run BCryptBenchmark $DATA
+php out/php/index.php $DATA
+neko out/BCryptBenchmark.n $DATA
+python3 out/BCryptBenchmark.py $DATA
+# lua out/BCryptBenchmark.lua $DATA
---
+++ benchmarkSHA512.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source benchmark.env
+
+readonly DATA=$*
+
+./out/cpp/Sha512Benchmark $DATA
+./out/cppGCGen/Sha512Benchmark $DATA
+node out/Sha512Benchmark.js $DATA
+node out/Sha512Benchmark.es6.js $DATA
+java -jar out/java/Sha512Benchmark.jar $DATA
+java -jar out/jvm/Sha512Benchmark.jar $DATA
+hl out/Sha512Benchmark.hl $DATA
+./out/hl/Sha512Benchmark $DATA
+mono out/cs/bin/Sha512Benchmark.exe $DATA
+haxe build/common.hxml --run Sha512Benchmark $DATA
+php out/php/index.php $DATA
+neko out/Sha512Benchmark.n $DATA
+python3 out/Sha512Benchmark.py $DATA
+# lua out/Sha512Benchmark.lua $DATA
